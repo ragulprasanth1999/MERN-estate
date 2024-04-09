6168e9dc98011fc1669440ab440810c1f0e37c34
ragulprasanth1999 <rahulprasanth834@gmail.com>
2024-04-10 00:30:58 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter >
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/ sign-in" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
